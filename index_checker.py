5542df7b3bb142b941c71973bc7c6aa821e4346a
Alexis-Finch <138725981+Alexis-Finch@users.noreply.github.com>
2023-07-05 21:23:42 -0700

    Update index_checker.py
    Improved variable names, readability, commenting

--- index_checker.py
+++ index_checker.py
@@ -3,9 +3,18 @@ import os
 import requests
 import json
 
+# Author: Alexis Finch
+# Date: July 7, 2023
+#
+# The purpose of this program is to present a
+
+
+# Formats indexes for sake of readability. Converts decimals into percentages.
 def indexFormatter (index):
     return f"{index:.2%}"
 
+#Gets a system id (int)
+#Returns a system name (string)
 def getNameBySystemID (solar_system_id):
     system_url = 'https://esi.evetech.net/latest/universe/systems/' + repr(solar_system_id)
     return requests.get(system_url).json()["name"]
@@ -25,39 +34,57 @@ def GetIndices(alliance_IDs):
     index_response = requests.get(indices_url).json()
 
 
+    # Stores the data that will be sent at the end
+    alliance_indexes_lists = [{'name':'Manufacturing','data':[]},{'name':'Reactions', 'data':[]}]
 
-    index_list = [{'name':'Manufacturing','data':[]},{'name':'Reactions', 'data':[]}]
-
+    #Reads through all indexes returned by ESI
     for cost_index in index_response:
+        #Then checks the list of system IDs in the alliance systems list, to see if it's on the list
         for system in alliance_systems:
+            # If the systems match (cost index pertains to a system on the alliance_systems list
             if cost_index['solar_system_id'] == system:
-                index_list[0]['data'].append({'solar_system_id':cost_index['solar_system_id'], 'cost_index':cost_index['cost_indices'][0]['cost_index']})
-                index_list[1]['data'].append({'solar_system_id':cost_index['solar_system_id'], 'cost_index':cost_index['cost_indices'][5]['cost_index']})
+                # then it adds the indices to the output data, formatted with system ID and cost index
+                alliance_indexes_lists[0]['data'].append({'solar_system_id':cost_index['solar_system_id'], 'cost_index':cost_index['cost_indices'][0]['cost_index']})
+                alliance_indexes_lists[1]['data'].append({'solar_system_id':cost_index['solar_system_id'], 'cost_index':cost_index['cost_indices'][5]['cost_index']})
 
-    index_list[0]['data'] = sorted(index_list[0]['data'], key=operator.itemgetter('cost_index'))
-    index_list[1]['data'] = sorted(index_list[1]['data'], key=operator.itemgetter('cost_index'))
 
-    index_list[0]['data'].reverse()
-    index_list[1]['data'].reverse()
+    # Sorts the lists into ascending order
+    alliance_indexes_lists[0]['data'] = sorted(alliance_indexes_lists[0]['data'], key=operator.itemgetter('cost_index'))
+    alliance_indexes_lists[1]['data'] = sorted(alliance_indexes_lists[1]['data'], key=operator.itemgetter('cost_index'))
 
+
+    #Reverses the lists of indexes to descending order
+    alliance_indexes_lists[0]['data'].reverse()
+    alliance_indexes_lists[1]['data'].reverse()
+
+    #String for the final output
     output_string = ""
-    for list in index_list:
+
+    #runs through the finalized data, each one is a different index type
+    for list in alliance_indexes_lists:
+        #add the header to the output
         output_string += (list['name'] + " Cost Index Report: ```")
+        # add each systems data
         for system in list['data']:
             if system['cost_index'] > 0.001:
                 output_string += (getNameBySystemID(system["solar_system_id"]) + ": " + str(indexFormatter(system['cost_index'])) + "\n")
+        # Add the end syntax for the list of systems
         output_string += "```\n\n"
 
+    #if the config file is true for slack
     if configuration['webhooks']['slack']:
-        print("Sending to slack!")
+        #Pulls the slack URL from the ENV Variable
         slack_url = os.environ["INDY_BOT_SLACK_WEBHOOK_URL"]
 
+        #Sends it in the proper format, to the slack webhook
         requests.post(slack_url, data=json.dumps({'text': output_string}), headers={'Content-Type': 'application/json'})
 
+    #if the config file is true for discord
     if configuration['webhooks']['discord']:
-        print("Sending to discord!")
+        #Pulls the discord webhook URL from the ENV variable
         discord_url = os.environ['INDY_BOT_DISCORD_WEBHOOK_URL']
 
+        #Sends it in the proper format, to the discord webhook
         requests.post(discord_url, data=json.dumps({'content': output_string}), headers={'Content-Type': 'application/json'})
 
 #opens config file
@@ -66,4 +93,5 @@ configFile = open("./config.json")
 #returns JSON object as a dictionary
 configuration = json.load(configFile)
 
+#Calls the function to fetch, and send, the indexes
 GetIndices(configuration["alliance_IDs"])
