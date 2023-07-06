50ceb144d1c6fa1e84e214429183550657bb707d
Alexis-Finch <138725981+Alexis-Finch@users.noreply.github.com>
2023-07-06 13:39:03 -0700

    Update index_checker.py
    Reverted to two-post reporting structure.
    
    Added region filtering.

--- index_checker.py
+++ index_checker.py
@@ -18,42 +18,64 @@ def indexFormatter (index):
     return f"{index:.2%}"
 
 def parseIndicesBySystemList (index_response, alliance_systems):
-    system_indices_list = []
-
+    indices_list = [[],[]]
     #Reads through all indexes returned by ESI
+
     for cost_index in index_response:
         #Then checks the list of system IDs in the alliance systems list, to see if it's on the list
         for system in alliance_systems:
             # If the systems match (cost index pertains to a system on the alliance_systems list
             if cost_index['solar_system_id'] == system['id']:
-                sum = 0
-                for index in cost_index['cost_indices']:
-                   sum += index['cost_index']
-                if sum > (configuration['display_threshold'] * 6):
-                    system_indices_list.append([system['name'],cost_index['cost_indices'][0]['cost_index'],cost_index['cost_indices'][5]['cost_index']])
+
+                if cost_index['cost_indices'][0]['cost_index'] > configuration['display_threshold']:
+                    indices_list[0].append([system['name'],cost_index['cost_indices'][0]['cost_index']])
+
+                if cost_index['cost_indices'][5]['cost_index'] > configuration['display_threshold']:
+                    indices_list[1].append([system['name'],cost_index['cost_indices'][5]['cost_index']])
 
     # sorts the data
-    system_indices_list.sort(key=lambda x: x[1])
+    indices_list[0].sort(key=lambda x: x[1])
+    indices_list[0].reverse()
 
-    system_indices_list.reverse()
+    indices_list[1].sort(key=lambda x: x[1])
+    indices_list[1].reverse()
 
-    for list_item in system_indices_list:
-        list_item[1] = indexFormatter(list_item[1])
-        list_item[2] = indexFormatter(list_item[2])
+    return indices_list
 
-    system_indices_list.insert(0,['System','Manuf','React'])
+def buildOutputString (indices_list):
+    outputString = 'Manufacturing Cost Index Report: ```'
 
-    return system_indices_list
+    for system in indices_list[0]:
+        system[1] = indexFormatter(system[1])
 
-def buildOutputString (system_indices_list):
-    outputString = "System Industry Indices Report \n ```"
+        outputString += ('{0:7} {1:>8}'.format(*system)) + "\n"
 
-    for col in system_indices_list:
-        outputString += ('{0:7} {1:>8} {2:>8}'.format(*col)) + "\n"
+    outputString += "``` \n"
+    outputString += 'Reaction Cost Index Report: ```'
 
+    for system in indices_list[1]:
+        system[1] = indexFormatter(system[1])
+
+        outputString += ('{0:7} {1:>8}'.format(*system)) + "\n"
     outputString += "```"
+
     return outputString
 
+def filterByRegions (alliance_systems):
+    inRegionFilteredList = []
+    resolve_region_id_url = 'https://esi.evetech.net/latest/universe/regions/'
+    resolve_constellation_id_url = 'https://esi.evetech.net/latest/universe/constellations/'
+
+    for region in configuration['regions']:
+        regionJSON = requests.get(resolve_region_id_url+str(region)).json()
+        for constellation in regionJSON['constellations']:
+            constellationJSON = requests.get(resolve_constellation_id_url+str(constellation)).json()
+            for system in constellationJSON['systems']:
+                for trackedSystem in alliance_systems:
+                    if system == trackedSystem['id']:
+                        inRegionFilteredList.append(trackedSystem)
+
+    return inRegionFilteredList
 
 def postSlackWebhook (content):
     #Pulls the slack URL from the ENV Variable
@@ -83,6 +105,8 @@ def GetIndices(alliance_IDs):
 
     resolve_system_name_url = 'https://esi.evetech.net/latest/universe/names/'
     alliance_systems = requests.post(resolve_system_name_url, headers={'Accept': 'application/json','Content-Type': 'application/json','Cache-Control': 'no-cache'}, json=alliance_systems).json()
+
+    alliance_systems = filterByRegions(alliance_systems)
 
     index_response = requests.get(indices_url).json()
 
